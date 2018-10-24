59db42d2e8b49cdc949d9b210ae5702c5e0890aa
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-24 00:02:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const reassignLeastFavorite = "some value"
+
+function changeLeastFavoriteCustomer() {
+  reassignLeastFavorite = "new value"
+}
+
+const favoriteCustomer = "some"
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "nothing"
+  let favoriteCustomer = "something"
+}
